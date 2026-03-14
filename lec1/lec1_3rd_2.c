a0d963afbae63de35ab1d15aaaac30ca100d578b
SungHyuk <127981454+SungHyuk108@users.noreply.github.com>
2026-03-14 13:48:00 +0000

    change_after_lec1

--- lec1/lec1_3rd_2.c
+++ lec1/lec1_3rd_2.c
@@ -4,11 +4,12 @@ int main() {
     int a = 2;
     float a2 = 2.0;
     int b = 3;
-    float b2 = 3.0;
+    float c = 3.0;
 
     // float나 double은 % 연산이 불가능하다! (정수형에서만 % 연산이 가능함!) 그래서 a2와 b2는 float이기에 % 연산이 불가능함!
 
-    printf("a + a2 = %f", a+a2);
-    printf("b + b2 = %f", b+b2);
+    printf("a %% b = %d\n", a%b);
+    printf("a %% c (c cast to int): %d\n", a%(int)c);
+    printf("a2 %% b (a2 cast to int): %d\n", (int)a2 % b);
 
 }
